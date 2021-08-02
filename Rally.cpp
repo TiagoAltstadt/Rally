2b9a5d4b39c4c025b10aa6f15899ca54d16f1768
TiagoAltstadt <tiagoaltstadt@gmail.com>
2021-08-02 18:25:26 -0300

    first commit

---
+++ Rally.cpp
@@ -0,0 +1,14 @@
+
+
+main() {
+    //declare ud. las variables de esta función y su inicialización, si corresponde.
+    Abrir (EtapasRally);	    // Dentro del módulo ProcEtapasRally, invocar a LeeretapsRally.
+                                                       // Dentro de los módulos de los listados si corresponde     
+    ProcEtapasRally(argumentos…);   // se deberá invocar al módulo de OrdxBur.
+    ListadoCorredores(argumentos…);  // En el módulo ListadoCorredores debe existir un 
+    ListadoLargada(argumentos…);      // freopen(nomFisArchivo, modo, stdout);
+    ListadoPuestosFinal(argumentos…);  // En el módulo ListadoGanadoresxTipoVehic debe
+    ListadoGanadoresxTipoVehic(argumentos…);  // volverse el freopen a la consola.
+    Cerrar (EtapasRally);   
+    return 0;
+} //main
